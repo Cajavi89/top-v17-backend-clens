c758d71e24fd421ce722b96279b47d6ccc3cea41
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-09 23:54:06 -0500

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const reviews = [
   {
@@ -138,6 +139,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // GET /
 app.get("/", (request, response) => {
