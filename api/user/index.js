49adbe1ba794e0bf381fd63cc08df312c4af7b23
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-14 18:59:22 -0500

    is autheticated token validation

--- api/user/index.js
+++ api/user/index.js
@@ -9,12 +9,13 @@ const {
   getUserByEmailHandler,
 } = require('./user.controller');
 
+const { isAuthenticated, hasRole } = require('../../auth/auth.service');
 const router = Router();
 
 router.get('/', getAllUsersHandler);
 router.post('/', createUserHandler);
 router.get('/:id', getUserByIdHandler);
-router.get('/email/:email', getUserByEmailHandler);
+router.get('/email/:email', isAuthenticated(), getUserByEmailHandler);
 router.patch('/:id', updateUserHandler);
 router.delete('/:id', deleteUserHandler);
 
