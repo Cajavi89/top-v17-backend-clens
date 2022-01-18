a6611011e6dc234bac6adc44c6a2f68762aabb5f
Gilbert Huarcaya Matias <85653232+GilbertHuarcaya@users.noreply.github.com>
2022-01-18 18:24:27 -0500

    fixed conflicts

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const order = require('./api/order');
 const review = require('./api/review');
 const authLocal = require('./auth/local');
 const payment = require('./api/payment');
+const upload = require('./api/upload');
 
 // defining routes
 function routes(app) {
@@ -15,6 +16,7 @@ function routes(app) {
   app.use('/api/orders', order);
   app.use('/api/reviews', review);
   app.use('/api/payments', payment);
+  app.use('/api/uploads', upload);
 
   // auth routes
   app.use('/auth/local', authLocal);
