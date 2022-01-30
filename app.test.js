99e69da16824d91c74e4f27ba4e68e5c290260a0
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-30 18:10:40 -0500

    new testing

--- app.test.js
+++ app.test.js
@@ -1,8 +1,12 @@
 const request = require('supertest');
-const mongoose = require("mongoose");
-const User = require("./api/user/user.model");
+const mongoose = require('mongoose');
+const epayco = require('epayco-sdk-node')({
+  apiKey: process.env.E_PublicKey,
+  privateKey: process.env.E_PrivateKey,
+  lang: 'ES',
+  test: true
+})
 const app = require('./app');
-
 
 describe('/api/clens', () => {
   test('GET responds with success code', async () => {
@@ -10,40 +14,9 @@ describe('/api/clens', () => {
     expect(response.statusCode).toBe(200);
   });
 });
-describe('/api/users', () => {
-  test('GET responds with success code', async () => {
-    const response = await request(app).get('/api/users');
-    expect(response.statusCode).toBe(200);
-  });
-});
-describe('/api/orders', () => {
-  test('GET responds with success code', async () => {
-    const response = await request(app).get('/api/orders');
-    expect(response.statusCode).toBe(200);
-  });
-});
-describe('/api/services', () => {
-  test('GET responds with success code', async () => {
-    const response = await request(app).get('/api/services');
-    expect(response.statusCode).toBe(200);
-  });
-});
-describe('/api/reviews', () => {
-  test('GET responds with success code', async () => {
-    const response = await request(app).get('/api/reviews');
-    expect(response.statusCode).toBe(200);
-  });
-});
-describe('/api/uploads', () => {
-  test('GET responds with success code', async () => {
-    const response = await request(app).get('/api/uploads');
-    expect(response.statusCode).toBe(200);
-  });
-});
 
-
-describe("GET /privateRoute", () => {
-  test("gives true if not authenticated/ authorized", async () => {
+describe('GET /privateRoute', () => {
+  test('gives true if not authenticated/ authorized', async () => {
     const response = await request(app).get('/api/users/email/test@mail');
     expect(response.unauthorized).toBe(true);
   });
